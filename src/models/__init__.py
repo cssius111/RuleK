8daa74e26bd50cdb2b17ac483853650d4c57ce88
Pinle Chen <90313887+cssius111@users.noreply.github.com>
2025-07-29 14:43:50 +0800

    fix import error by removing nonexistent NPCTrait

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,6 +2,6 @@
 模型子包统一导出
 """
 
-from .npc import NPC, NPCTrait   # noqa: F401
+from .npc import NPC  # noqa: F401
 from .rule import Rule           # noqa: F401
 from .event import Event, EventType  # 新增导出  # noqa: F401
